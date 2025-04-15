4216cf7ec9c25c6d5e0038e3543f8cb805bb4f39
Vamaa-shah <vamashah.1123@gmail.com>
2025-04-14 20:26:31 -0400

    Add working CDK code with DynamoDB and S3

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // DynamoDB Table
+    new dynamodb.Table(this, 'FeedbackTable', {
+      tableName: 'FeedbackTable',
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY // For testing
+    });
+
+    // S3 Bucket
+    new s3.Bucket(this, 'FrontendBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY // For testing
+    });
+  }
+}
